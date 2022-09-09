3f38427bd1bd9f221698695e906f114419649d9b
Aldo <novioatomico94@gmail.com>
2022-09-09 00:02:01 -0500

    Funciona Correctamente

--- Practica4/src/ListaD.java
+++ Practica4/src/ListaD.java
@@ -34,6 +34,16 @@ public class ListaD {
                 ultimo.sig = primero;
                 primero.ant = ultimo;
             }
+           else {
+               NodoD aux = primero;
+               while ((aux.info<=x)&&(aux!=ultimo))
+                   aux=aux.sig;
+               aux.ant.sig=temp;
+               temp.ant=aux.ant;
+               aux.ant=temp;
+               temp.sig=aux;
+            }
+
            /* else {
                 NodoD ant;
                 NodoD aux;
@@ -53,23 +63,49 @@ public class ListaD {
         NodoD aux = raiz;
         aux = raiz.sig;
         raiz = aux;
-        System.out.print(raiz.info + " ");
+        //System.out.print(raiz.info + " ");
     }
 
     public void recorrerIzquierda(){
        NodoD aux = raiz;
        aux = raiz.ant;
        raiz = aux;
-       System.out.print(aux.info + " ");
+      // System.out.print(aux.info + " ");
 
     }
 
-    /*public int eliminar( int y ){
-        NodoD aux = raiz;
-        NodoD an = null;
-        NodoD si = null;
+    public void eliminar(int y){
+        NodoD aux;
+        //aux = primero;
+        if (primero.info == y){
+            primero=primero.sig;
+            primero.ant=ultimo;
+            ultimo.sig=primero;
+            return;
+        }
 
-    }*/
+
+
+        if(ultimo.info==y){
+            ultimo=ultimo.ant;
+            ultimo.sig=primero;
+            primero.ant=ultimo;
+            return;
+        }
+
+        aux = primero.sig;
+        while (aux != ultimo){
+            if (y==aux.info){
+                aux.ant.sig=aux.sig;
+                aux.sig.ant=aux.ant;
+                aux.ant=null;
+                aux.sig=null;
+                return;
+            }
+            aux=aux.sig;
+        }
+        System.out.println("El dato no existe");
+    }
 
     public void mostrar(){
         raiz = primero;
@@ -80,5 +116,20 @@ public class ListaD {
             raiz = aux;
         }
         System.out.println(raiz.info);
+    }
+
+    public void mostrarRaiz(){
+        NodoD aux = raiz;
+        System.out.println("Esta es la raiz de la cola: "+ raiz.info);
+        //System.out.println(raiz.info);
+    }
+
+    public void  prueba(){
+        NodoD aux = raiz;
+        System.out.println("datos de raiz: "+aux.info);
+        aux = raiz.sig;
+        System.out.println("Datos despues de raiz: "+aux.info);
+        aux = raiz.sig.sig;
+        System.out.println("Dos veces de raiz: "+ aux.info);
     }
 }
--- Practica4/src/TestPrueba.java
+++ Practica4/src/TestPrueba.java
@@ -1,13 +1,14 @@
 public class TestPrueba {
     public static void main(String[] args) {
         ListaD l = new ListaD();
-        l.insertar(4);
-        l.insertar(3);
+       l.insertar(3);
         l.insertar(2);
-        l.insertar(99);
+        l.insertar(6);
+        l.insertar(5);
         l.insertar(100);
-        l.insertar(1);
-        //l.insertar(1);
+        l.eliminar(2);
+       // l.eliminar(3);
         l.mostrar();
+        l.mostrarRaiz();
     }
 }
